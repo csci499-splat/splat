c9416910d6b1755c06b5f2ce35759233ccdd292d
having11 <gatoninja236@gmail.com>
2021-10-02 15:22:11 -0500

    Added staff page with navigation skeleton

---
+++ ClientApp/src/scenes/staff/pages/Reports.tsx
@@ -0,0 +1,19 @@
+import React, { FC, ReactElement } from 'react';
+import { IStaffChild } from '../Staff';
+
+interface ReportProps extends IStaffChild {
+    
+}
+
+const Reports: FC<ReportProps> = (props: ReportProps): ReactElement => {
+
+
+
+    return (
+        <>
+        <h1>{props.pageName}</h1>
+        </>
+    )
+};
+
+export default Reports;
