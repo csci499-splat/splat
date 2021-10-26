bf1526f6745e6cf736fcdfd2e4f269319e3ec606
Padraic Heberlein <pheberle@uwsuper.edu>
2021-10-26 18:53:23 -0500

    Created the TrendsReport.cs model.

---
+++ splat/Models/TrendsReportcs.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace splat.Models
+{
+    public class TrendsReportcs
+    {
+        public Item[] trendItems { get; set; }
+    }
+}
