a95a169210ae6654d07e2bd91664265b60c7cb5d
Enrique Lazaro Sanchez <elazaros@uwsuper.edu>
2021-10-28 17:50:53 -0500

    TotalReports controllerworks except from that it gets the info from all the pickups instead of only the disbursed ones.

--- splat.Tests/TotalReportsTest.cs
+++ splat.Tests/TotalReportsTest.cs
@@ -7,49 +7,118 @@ using System.Collections.Generic;
 
 namespace splat.Tests
 {
-    /*
+    
     public class TotalReportsTest
     {
         [Fact]
         public void GetTotalWeights_SumOfWeights()
         {
-
+            var totalWeights = TotalReportsController.GetTotalWeight2(GeneratePickups().AsQueryable());
+            var expectedTotalWeights = 48.5;
+            Assert.StrictEqual(expectedTotalWeights, totalWeights);
         }
 
-            });
-            pickupsList.Add(new Pickup
-            {
-                Id = Guid.NewGuid(),
-                Weight = 2.5,
-                PickupStatus = PickupStatus.PENDING,
-                PickupTime = null,
-                CanceledTime = null,
-                StudentInfo = new Student
-                {
-                    StudentId = "1311111",
-                    Age = 24,
-                    OnMealPlan = false
-                },
-                SubmittedAt = new DateTime()
-
-            });
-            pickupsList.Add(new Pickup
-            {
-                Id = Guid.NewGuid(),
-                Weight = 25.5,
-                PickupStatus = PickupStatus.DISBURSED,
-                PickupTime = new DateTime(2020, 05, 12, 6, 22, 0),
-                CanceledTime = null,
-                StudentInfo = new Student
-                {
-                    StudentId = "1111111",
-                    Age = 22,
-                    OnMealPlan = false
-                },
-                SubmittedAt = new DateTime()
-
-            });
-            return pickupsList.AsQueryable();
+        [Fact]
+        public void GetTotalDisbursements_CountPickups()
+        {
+            var totalDisbursements = TotalReportsController.TotalDisbursements2(GeneratePickups().AsQueryable());
+            var expectedCount = 3;
+            Assert.StrictEqual(expectedCount, totalDisbursements);
         }
-    }*/
+
+        [Fact]
+        public void GetTotalPeopleImpacted_SumStudendsWithHouseholdInfo()
+        {
+            var totalPeople = TotalReportsController.TotalPeopleImpacted2(GeneratePickups().AsQueryable());
+            var expectedCount = 8;
+            Assert.StrictEqual(expectedCount, totalPeople);
+        }
+
+        [Fact]
+        public void GetTotalRecurringVisits_SumOfRepeatedIds()
+        {
+            var totalRecurringVisits= TotalReportsController.TotalRecurringVisits2(GeneratePickups().AsQueryable());
+            var expectedCount = 1;
+            Assert.StrictEqual(expectedCount, totalRecurringVisits);
+        }
+
+        [Fact]
+        public void GetTotalIndividualVisits_SumOfRepeatedIds()
+        {
+            var totalIdividualVisits = TotalReportsController.TotalIndividualVisits2(GeneratePickups().AsQueryable());
+            var expectedCount = 2;
+            Assert.StrictEqual(expectedCount, totalIdividualVisits);
+        }
+
+        private List<Pickup> GeneratePickups()
+        {
+            return new List<Pickup>()
+            {
+                new Pickup
+                {
+                    Id = Guid.NewGuid(),
+                    Weight = 2.5,
+                    PickupStatus = PickupStatus.PENDING,
+                    PickupTime = null,
+                    CanceledTime = null,
+                    StudentInfo = new Student
+                    {
+                        StudentId = "1311111",
+                        Age = 24,
+                        OnMealPlan = false
+                    },
+                    SubmittedAt = new DateTime(),
+                    HouseholdInfo = new HouseholdInfo
+                    {
+                        NumAdults = 2,
+                        NumMinors = 0,
+                        NumSeniors = 0,
+                    }
+                },
+                new Pickup
+                {
+                    Id = Guid.NewGuid(),
+                    Weight = 25.5,
+                    PickupStatus = PickupStatus.DISBURSED,
+                    PickupTime = new DateTime(2020, 05, 12, 6, 22, 0),
+                    CanceledTime = null,
+                    StudentInfo = new Student
+                    {
+                        StudentId = "1111111",
+                        Age = 22,
+                        OnMealPlan = false
+                    },
+                    SubmittedAt = new DateTime(),
+                    HouseholdInfo = new HouseholdInfo
+                    {
+                        NumAdults = 2,
+                        NumMinors = 0,
+                        NumSeniors = 0,
+                    }
+                },
+                new Pickup
+                {
+                    Id = Guid.NewGuid(),
+                    Weight = 20.5,
+                    PickupStatus = PickupStatus.DISBURSED,
+                    PickupTime = new DateTime(2020, 05, 12, 6, 22, 0),
+                    CanceledTime = null,
+                    StudentInfo = new Student
+                    {
+                        StudentId = "1111111",
+                        Age = 22,
+                        OnMealPlan = false
+                    },
+                    SubmittedAt = new DateTime(),
+                    HouseholdInfo = new HouseholdInfo
+                    {
+                        NumAdults = 2,
+                        NumMinors = 0,
+                        NumSeniors = 0,
+                    }
+                }
+            };
+        }
+
+    }
 }
--- splat/Controllers/TotalReportsController.cs
+++ splat/Controllers/TotalReportsController.cs
@@ -46,9 +46,19 @@ namespace splat.Controllers
             return (double)await pickups.SumAsync(p => p.Weight);
         }
 
+        public static double GetTotalWeight2(IQueryable<Pickup> pickups)
+        {
+            return (double) pickups.Sum(p => p.Weight).Value;
+        }
+
         public async Task<int> TotalDisbursements(IQueryable<Pickup> pickups)
         {
             return await pickups.CountAsync();
+        }
+
+        public static int TotalDisbursements2(IQueryable<Pickup> pickups)
+        {
+            return pickups.Count();
         }
 
         public async Task<int> TotalPeopleImpacted(IQueryable<Pickup> pickups)
@@ -56,6 +66,14 @@ namespace splat.Controllers
             var students = await pickups.GroupBy(p => p.StudentInfo.StudentId).CountAsync();
             var result = await pickups
                 .SumAsync(p => p.HouseholdInfo.NumAdults + p.HouseholdInfo.NumMinors + p.HouseholdInfo.NumSeniors);
+            return result + students;
+        }
+
+        public static int TotalPeopleImpacted2(IQueryable<Pickup> pickups)
+        {
+            var students = pickups.GroupBy(p => p.StudentInfo.StudentId).Count();
+            var result = pickups
+                .Sum(p => p.HouseholdInfo.NumAdults + p.HouseholdInfo.NumMinors + p.HouseholdInfo.NumSeniors);
             return result + students;
         }
 
@@ -67,11 +85,26 @@ namespace splat.Controllers
                 .CountAsync();
         }
 
+        public static int TotalRecurringVisits2(IQueryable<Pickup> pickups)
+        {
+            return pickups
+                .GroupBy(p => p.StudentInfo.StudentId)
+                .Where(p => p.Count() > 1)
+                .Count();
+        }
+
         public Task<int> TotalIndividualVisits(IQueryable<Pickup> pickups)
         {
             return pickups
                 .GroupBy(p => p.StudentInfo.StudentId)
                 .CountAsync();        
+        }
+
+        public static int TotalIndividualVisits2(IQueryable<Pickup> pickups)
+        {
+            return pickups
+                .GroupBy(p => p.StudentInfo.StudentId)
+                .Count();
         }
     }
 }
