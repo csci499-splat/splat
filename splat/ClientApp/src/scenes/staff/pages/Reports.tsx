84f83a3fd4069e6b3f176b5b4dba14de1e4017a8
tom116458 <tom116458@outlook.com>
2021-11-04 12:08:37 +0800

    fix errors and add date verification

--- splat/ClientApp/src/scenes/staff/pages/Reports.tsx
+++ splat/ClientApp/src/scenes/staff/pages/Reports.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from 'react';
 import { DatePicker, LocalizationProvider} from '@mui/lab';
-import { Button, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
+import { Button, Grid, MenuItem, Select, SelectChangeEvent, TextField } from '@mui/material';
 import { IStaffChild } from '../Staff';
 import {TotalReport} from '../../../models/TotalReport';
 import { useState } from 'react';
@@ -12,7 +12,7 @@ import MobileDatePicker from '@mui/lab/MobileDatePicker';
 import FormControl from '@mui/material/FormControl';
 import TotalReportDialog from '../subcomponents/TotalReportDialog';
 import TrendReportDialog from '../subcomponents/TrendReportDialog';
-
+import { TrendReport } from '../../../models/TrendReport';
 
 interface ReportProps extends IStaffChild {
     
@@ -20,30 +20,39 @@ interface ReportProps extends IStaffChild {
 
 const Reports: FC<ReportProps> = (props: ReportProps): ReactElement => {
     
-    const [startDateValue, setStartDateValue] = React.useState<Date | null>(new Date());
-    const [endDateValue, setEndDateValue] = React.useState<Date | null>(new Date());
-    const [type, setType] = React.useState('');
-
+    const [startDateValue, setStartDateValue] = React.useState<Date | null>();
+    const [endDateValue, setEndDateValue] = React.useState<Date | null>();
+    //TODO Create a new type, if user want to add new report type, they don't need to change 3 lines. 
+    const [reportType, setReportType] = React.useState<'totalReport' | 'trendReport'|''>('');
     const [dialogOpen, setDialogOpen] = useState({totalReport: false, trendReport: false});
     
 
-    const handleDialogOpen = (dialog: 'totalReport' | 'trendReport') => {
+    const handleDialogOpen = (dialog: 'totalReport' | 'trendReport'|'') => {
         setDialogOpen((prevState) => ({ ... prevState, [dialog]: true}));
         
     };
-    const handleDialogClose = (dialog: 'totalReport' | 'trendReport') => {
+    const handleDialogClose = (dialog: 'totalReport' | 'trendReport'|'') => {
         setDialogOpen((prevState) => ({ ...prevState, [dialog]: false}))
     };
     const handleTypeChange = (event: SelectChangeEvent) => {
-        setType(event.target.value as string);
+        setReportType(event.target.value as 'totalReport' | 'trendReport'|'');
     };
     
     return (
         <>
-        <FormControl variant="standard" sx={{ m: 1, minWidth: 120 }}>
+        
+        <Box
+        sx={{
+            textAlign: 'center',
+            position: 'absolute',
+            left: '30%',
+
+          }}
+        >
         <h1>{props.pageName}</h1>
-            <Stack spacing={3}>
-            <MobileDatePicker
+        <Stack direction="row" spacing={2}  >
+
+        <MobileDatePicker
             label="Select start date"
             value={startDateValue}
             maxDate={endDateValue}
@@ -52,39 +61,32 @@ const Reports: FC<ReportProps> = (props: ReportProps): ReactElement => {
             }}
             renderInput={(params) => <TextField {...params} />}
             />
-            <Box sx = {{ mx:2}}> to </Box>
+            <Box sx = {{alignItems: 'center',fontSize: 21}}> To </Box>
             <MobileDatePicker
             label="Select end date"
             value={endDateValue}
             minDate= {startDateValue}
-            maxDate={endDateValue}
+            
+            maxDate={new Date()}
             onChange={(newEndDateValue) => {
                 setEndDateValue(newEndDateValue)
             }}
-            renderInput={(params) => <TextField {...params} />}
+            renderInput={(params) => <TextField {...params} 
+            disabled={!Boolean(startDateValue)}
+            />}
             />
-            </Stack>
-
 
         <Select
         id="reportType"
-        value={type}
+        value={reportType}
         onChange={handleTypeChange}
-        label="Type"
+        label="Report Type"
+        disabled={Boolean(startDateValue)||Boolean(endDateValue)}
         >
-            <MenuItem value="">
-                <em>None</em>
+            <MenuItem value="totalReport">Total Report
             </MenuItem>
-            <MenuItem value="totalReport" 
-            onClick={() => handleDialogOpen('totalReport')}
-
-            >Total Report
-            </MenuItem>
-            <MenuItem value="trendReport"
-            onClick={() => handleDialogOpen('trendReport')}          
-            >Trend Report</MenuItem>
+            <MenuItem value="trendReport">Trend Report</MenuItem>
         </Select>
-        </FormControl>
         <TotalReportDialog
         open={dialogOpen.totalReport}
         onClose={() => handleDialogClose('totalReport')}
@@ -93,6 +95,15 @@ const Reports: FC<ReportProps> = (props: ReportProps): ReactElement => {
         open={dialogOpen.trendReport}
         onClose={() => handleDialogClose('trendReport')}
         />
+        <Button
+        variant="contained"
+        disabled={Boolean(startDateValue)||Boolean(endDateValue)}
+        onClick={()=> handleDialogOpen(reportType)}
+        >Open</Button>
+        </Stack>
+        </Box>
+
+
         </>
     )
 };
