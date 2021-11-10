1c022b9396ad8bfec8d7fd1c1760f10dee075a01
(tom116458) <(tom116458@outlook.com)>
2021-11-10 08:48:20 -0600

    Add recharts, line chart done.

--- splat/ClientApp/src/scenes/staff/subcomponents/TrendReportDialog.tsx
+++ splat/ClientApp/src/scenes/staff/subcomponents/TrendReportDialog.tsx
@@ -1,10 +1,11 @@
 import { Dialog, DialogContent,DialogActions, DialogTitle,
-    Paper, TableBody, TableContainer, TableHead, TableRow, Tooltip,Typography, Button } from '@mui/material';
+    Paper, TableBody, TableContainer, TableHead, TableRow,Typography, Button } from '@mui/material';
 import React, { FC, ReactElement, useState } from 'react';
 import TableCell from '@mui/material/TableCell';
 import Table from '@mui/material/Table';
 import { baseRequest } from '../../../services/api/genericRequest';
 import {TrendReport} from '../../../models/TrendReport'
+import { LineChart, Line, XAxis, YAxis, Tooltip, Label, Legend, CartesianGrid } from 'recharts';
 
 const TrendReportTest: TrendReport = {
     entries: [
@@ -31,14 +32,20 @@ const TrendReportTest: TrendReport = {
     ]
 }
 
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
+    });
+};
+
 type TrendReportDialogProps = {
     open: boolean;
     onClose: () => void;
 }
 const TrendReportDialog: FC<TrendReportDialogProps> = (props:TrendReportDialogProps) : ReactElement => {
-    const [trendReport, setTotalReport] = useState<TrendReport>();
+    const [trendReport, setTotalReport] = useState<TrendReport[]>([]);
     const getTotalReport = async () => {
-        let res = await baseRequest.get<TrendReport> ('/trendReport');
+        let res = await baseRequest.get<TrendReport[]> ('/trendReport');
         setTotalReport(res.data);
     };
     React.useEffect(() => {
@@ -57,28 +64,28 @@ const TrendReportDialog: FC<TrendReportDialogProps> = (props:TrendReportDialogPr
         >
             <DialogTitle>TrendReport</DialogTitle>
             <DialogContent>
-                <TableContainer component={Paper}>
-                    <Table sx = {{ maxWidth: 400, maxHeight: 600, alignItems: 'left'}} aria-label="trend report">
-                        <TableHead sx={{alignItems: 'center'}}>
-                            <TableRow sx={{alignItems: 'right'}}>
-                                <TableCell> Item Name </TableCell>
-                                <TableCell> Category </TableCell>
-                                <TableCell> Count </TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {TrendReportTest?.entries.map((row) => (
-                                <TableRow
-                                key={row.item.id}
-                                >
-                                <TableCell align="center">{row.item.name} </TableCell>
-                                <TableCell align="center">{row.item.category?.name} </TableCell>
-                                <TableCell align="center">{row.requestCount} </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                </TableContainer>
+                <LineChart
+                width={730}
+                height={250} 
+                data={trendReport}
+                margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+                >
+                    <XAxis dataKey="date">
+                    </XAxis>
+                    <YAxis type="number">
+                    <Label angle={270} position="center"
+                    style={{textAnchor: 'middle'}}>
+                        Item Count
+                    </Label>
+                    </YAxis>
+                    <Line
+                        type="monotone"
+                        stroke="#829ddf"
+                        dot={false}
+                        dataKey="requestCount"
+                    />
+
+                </LineChart>
             </DialogContent>
             <DialogActions sx={{margin: 1}}>
             <Button variant="outlined" onClick={props.onClose} color="secondary">Closed</Button>
