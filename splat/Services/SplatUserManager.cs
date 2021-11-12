b208efafd9ec797b1289892597c7c89e88de972d
Enrique Lazaro Sanchez <elazaros@uwsuper.edu>
2021-11-12 08:56:03 -0600

    SplatUserManager done. New Services folder made.

---
+++ splat/Services/SplatUserManager.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using splat.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace splat.Services
+{
+    public class SplatUserManager<TUser> : UserManager<TUser> where TUser : class
+    {
+        private readonly LDAPAuthenticationOptions _authenticationOptions;
+        public SplatUserManager(IUserStore<TUser> store, IOptions<IdentityOptions> optionsAccessor, IPasswordHasher<TUser> passwordHasher, IEnumerable<IUserValidator<TUser>> userValidators, IEnumerable<IPasswordValidator<TUser>> passwordValidators, ILookupNormalizer keyNormalizer, IdentityErrorDescriber errors, IServiceProvider services, ILogger<UserManager<TUser>> logger, IOptions<LDAPAuthenticationOptions> ldapOptions)
+            : base(store, optionsAccessor, passwordHasher, userValidators, passwordValidators, keyNormalizer, errors, services, logger)
+        {
+            _authenticationOptions = ldapOptions.Value;
+        }
+    }
+}
