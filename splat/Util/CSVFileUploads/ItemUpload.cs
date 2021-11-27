9daf26850a32fbb0ba7971c7961b1f50d786be22
Padraic Heberlein <pheberle@uwsuper.edu>
2021-11-27 13:58:42 -0600

    Started constructing the stubs for CategoryUpload.cs & ItemUplaod.cs

---
+++ splat/Util/CSVFileUploads/ItemUpload.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using splat.Models;
+
+namespace splat.Util.CSVFileUploads
+{
+    [Route("api/item/upload")]
+    [Authorize(Policy = "ElevatedRights", AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class ItemUpload
+    {
+        private readonly SplatContext _context;
+
+        public ItemUpload(SplatContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<bool>> GetRecords([FromBody] )
+        {
+            return new ActionResult<bool>(true);
+        }
+    }
+}
