a629819ecf0a0f6186fbb0c3e1acac663f4f6b77
John Masanzi <jmasanzi@uwsuper.edu>
2021-12-05 17:23:04 -0600

    Adds pickup cancellation email class that  contains a bodied message.

---
+++ splat/Services/Emails/PickupCancellationEmail.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+using splat.Models;
+using System.Text;
+using System.Web;
+
+namespace splat.Services.Emails
+{
+    public class PickupCancellationEmail : EmailBase
+    {
+        public PickupCancellationEmail(Pickup pickup, EmailExchangeOptions options) :
+            base(pickup, options)
+        { }
+
+        public override string GetMessageBody()
+        {
+            var body = new StringBuilder();
+            body.Append("<h3>Your requested pickup along with its item contents have been cancelled:</h3>");
+            body.Append("<table role=\"presentation\" width=\"100%\" border=\"1\" cellspacing=\"0\" " +
+                        "cellpadding=\"5\" style=\"border:1px solid black; border-collapse:collapse\">");
+            body.Append("<tr style=\"background-color:white\">");
+            body.Append("<th>Category</th><th>Item</th><th>Description</th><th>Quantity</th>");
+            body.Append("</tr>");
+
+            foreach (var entry in _pickup.ItemRequests)
+            {
+                body.AppendFormat("<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td></tr>",
+                                  entry.Item.Category.Name, entry.Item.Name, entry.Item.Description,
+                                  entry.Quantity);
+            }
+
+            body.Append("</table>");
+
+            body.Append("<p>If this cancellation was made in error please contact the Director of Student Development found below.");
+            body.Append("<p>Jennifer Bird<br>Director of Student Development<br>jbird2@uwsuper.edu<br>ph.715-394-8571<br>office. YellowJacket Union 145</p>");
+
+            return body.ToString();
+        }
+    }
+}
