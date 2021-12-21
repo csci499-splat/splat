881b5a280274ca49f5c65bbc26319d24c2b52851
John Masanzi <jmasanzi@uwsuper.edu>
2021-12-20 21:52:04 -0600

    absence make-up; Adds 4 categories to be used in pickups test

--- splat.Tests/TestCategories.cs
+++ splat.Tests/TestCategories.cs
@@ -40,5 +40,41 @@ namespace splat.Tests
             Description = "Stuff that's like bread!",
             Visible = true
         };
+
+        public static readonly Category Drink = new Category
+        {
+            Id = Guid.NewGuid(),
+            Name = "Drink",
+            Limit = 10,
+            Description = "A container containing consumable liquid 128oz and below",
+            Visible = true
+        };
+
+        public static readonly Category Candy = new Category
+        {
+            Id = Guid.NewGuid(),
+            Name = "Bread Stuffs",
+            Limit = 15,
+            Description = "a sweet food made with sugar or other sweeteners, typically formed in small, shaped pieces and flavored with chocolate, fruit, or nuts",
+            Visible = true
+        };
+
+        public static readonly Category Fruit = new Category
+        {
+            Id = Guid.NewGuid(),
+            Name = "Fruit",
+            Limit = 20,
+            Description = "the sweet and fleshy product of a tree or other plant that contains seed and can be eaten as food.",
+            Visible = true
+        };
+
+        public static readonly Category Dessert = new Category
+        {
+            Id = Guid.NewGuid(),
+            Name = "Dessert",
+            Limit = 5,
+            Description = "the sweet course eaten at the end of a meal",
+            Visible = true
+        };
     }
 }
